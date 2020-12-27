1ebea7a1b9d8555cb1affd7fcf0eeedee7624704
XDEmer0r-L0rd-360-G0d-SlayerXD <namisboss5@gmail.com>
2020-12-27 03:41:56 -0600

    Started to impliment custom bit classes to be used

---
+++ my_bits.py
@@ -0,0 +1,93 @@
+
+
+"""
+This was made specifically for working with the sha256 hash to better understand how it works
+"""
+
+
+class Bit:
+
+    def __init__(self, val: int):
+        self.val: int = val
+        self.history: str = f"<const>{self.val}</const>"
+
+
+class Array:
+    # All conversions assume hex was given for leading zeros
+    def __init__(self, bits=None):
+        self.content = []
+        if not bits:
+            self.content = []
+        elif type(bits) == str:
+            self.from_str(bits)
+        elif type(bits) == int:
+            self.from_num(bits)
+        elif type(bits) == list:
+            for a in bits:
+                self.content.append(self._gen_bit(a))
+        else:
+            raise ValueError("Bad init values")
+
+    def __len__(self):
+        return len(self.content)
+
+    def __iter__(self):
+        return (b for b in self.content)
+
+    def __getitem__(self, item):
+        return self.content[item]
+
+    @staticmethod
+    def _gen_bit(val):
+        # No safety checks in here
+        # Might be able to do it all in 1 line rn
+        if type(val) == int:
+            return Bit(val)
+        elif type(val) == str:
+            return Bit(int(val))
+
+    def from_str(self, text: str):
+        text = text.split("b")[-1].split('x')[-1]
+        for a in text:
+            self.content.append(self._gen_bit(a))
+        return self
+
+    def from_num(self, num):
+        val = bin(num).split('b')[-1]
+        val = '0' * ((4 - len(val) % 4) % 4) + val
+        for a in val:
+            self.content.append(self._gen_bit(a))
+        return self
+
+    def from_text(self, text):
+        for a in text:
+            self.from_num(ord(a))
+        return self
+
+    def to_str(self):
+        vals = ''
+        for a in self.content:
+            a: Bit
+            vals += str(a.val)
+        return vals
+
+    def shift_right(self, dist: int):
+        todo = self.content
+        self.content = []
+        important = todo[:-dist]
+        self.from_str("0" * dist)
+        for a in important:
+            self.content.append(a)
+        return self
+
+
+def main():
+    # This is for testing purposes
+    # print(bin(18))
+    data = Array().from_text('abc')
+    print(data.to_str())
+    print(data.shift_right(5).to_str())
+
+
+if __name__ == '__main__':
+    main()
